283a09fc793dc9a93f36aa8768e47afc243ba38e
Cecil Mahumane <cecmahumane@gmail.com>
2022-06-23 21:08:13 -0400

    Put search icon in search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import RedditLogo from '../images/RedditLogo.png'
+import Icon_search from '../images/Icon_search.png' 
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState('');
+
   return (
-    <div>
-        <h1>Header</h1>
+    <div className='navbar'>
+        <img src={RedditLogo} className="logo"/>
+        <div className='search-and-logo'>
+            <input
+                className='searchbar' 
+                type='search'
+                placeholder='Search'
+                />
+            <img src={Icon_search} className='searchIcon'/>
+        </div>
     </div>
   )
 }
