e721ab8679f4eb6248042b333f1858c46be7211d
Cecil Mahumane <cecmahumane@gmail.com>
2022-06-28 19:44:40 -0400

    Added trimTitle to Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Card(props) {
+  
+  const trimTitle = (props) => {
+    if (props.title.length > 70) {
+      return `${props.title.slice(0, 70)}...`
+    } else {
+      return props.title
+    }
+  }
+  
   return (
     <div className='card-box'>
         {(props.thumbnail !== "default" && props.thumbnail !== "self" && props.thumbnail !== "nsfw") && <div className='card-thumbnail' src={props.thumbnail}>
                   </div>}    
-        <p>{props.title}</p>
+        <p>{trimTitle(props)}</p>
         <p>{props.subredditNamePrefixed}</p>
     </div>
   )
