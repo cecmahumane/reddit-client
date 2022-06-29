c7d9294e4b9982ffd4162ea3c8ebdd7f6b66b295
Cecil Mahumane <cecmahumane@gmail.com>
2022-06-29 18:34:42 -0400

    Changed feed-item-content to CSS grid layout

--- src/components/RedditFeed.js
+++ src/components/RedditFeed.js
@@ -5,6 +5,8 @@ import Chat from '../images/Chat.png'
 
 function RedditFeed(props) {
 
+  console.log(props.thumbnail)
+
   return (
     <div className='feed-item'>
           <div className='feed-item-karma'>
@@ -13,20 +15,22 @@ function RedditFeed(props) {
             <img src={ArrowDown} alt='' className='arrowdown'/>
           </div>
           <div className='feed-item-content'>
-            <div className='feed-content-and-thumbnail'>
-              <div>
-                <p className='feed-item-content-info'><span className='feed-subredditNamePrefix'>{props.subredditNamePrefix}</span> <span className='feed-author'>* Posted by u/{props.author}</span></p>
-                <h3>{props.title}</h3>
-              </div>
-                <div className='thumbnail'>
-                  {/* {(props.thumbnail !== "default" && props.thumbnail !== "self" && props.thumbnail !== "nsfw") && <div className='feed-thumbnail' src={props.thumbnail}>
-                  </div>} */}
-                </div>
-              </div>
-              <div className='feed-item-content-comments'>
-                  <img src={Chat} alt=''/>
-                  <p>{props.numComments} Comments</p>
-              </div>
+            <div className='feed-item-header'>
+              <p className='feed-item-content-info'><span className='feed-subredditNamePrefix'>{props.subredditNamePrefix}</span> <span className='feed-author'>* Posted by u/{props.author}</span></p>
+            </div>
+            <h3 className='feed-item-title'>{props.title}</h3>
+            <div className='thumbnail'>
+              {(props.thumbnail !== "default" && props.thumbnail !== "self" && props.thumbnail !== "nsfw") && <div className='feed-thumbnail' src={props.thumbnail}>
+              </div>}
+              
+            </div>
+            <div className='feed-item-preview'>
+              
+            </div>
+            <div className='feed-item-content-comments'>
+                <img src={Chat} alt=''/>
+                <p>{props.numComments} Comments</p>
+            </div>
           </div>
     </div>
   )
