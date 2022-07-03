f14d4d9e724df3567ba6cccb0ff3958905d9c341
Cecil Mahumane <cecmahumane@gmail.com>
2022-07-03 18:23:33 -0400

    Changed popular post divs into buttons

--- src/components/PopularPosts.js
+++ src/components/PopularPosts.js
@@ -9,22 +9,17 @@ function PopularPosts(props) {
   return (
     <div>
         <div className='pp-box'>
-          <div className='pp-hot-box' onClick={() => props.toggleHotButton()}>
+          {/* <div className='pp-hot-box' onClick={() => props.toggleHotButton()}>
             <img className='pp-hot-image' src={Fire} alt=''/> 
             <p>Hot</p>
-          </div>
+          </div> */}
+          <button className='pp-hot-box' onClick={() => props.toggleHotButton()}><img className='pp-hot-image' src={Fire} alt=''/> Hot</button>
           <div className='pp-region-box'>
             <p>Canada</p>
             <img className='pp-region-arrow' src={CaretDown} alt=''/>
           </div>
-          <div className='pp-new-box' onClick={() => props.toggleNewButton()}>
-            <img className='pp-new-star' src={CircleWavy} alt=''/>
-            <p>New</p>
-          </div>
-          <div className='pp-top-box' onClick={() => props.toggleTopButton()}>
-            <img className='pp-top-graph' src={ChartBar} alt=''/>
-            <p>Top</p>
-          </div>
+          <button className='pp-new-box' onClick={() => props.toggleNewButton()}><img className='pp-new-star' src={CircleWavy} alt=''/> New</button>
+          <button className='pp-top-box' onClick={() => props.toggleTopButton()}><img className='pp-top-graph' src={ChartBar} alt=''/> Top</button>
           <div>
             <img className='pp-dots' src={ThreeDots} alt=''/>
           </div>
